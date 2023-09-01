0f3f0a76d7bf6d89ab6a9133903e8c04eca5a651
Sebastian0011 <sebastil@andrew.cmu.edu>
2023-09-01 14:40:49 -0400

    fixed fibroute

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,11 +1,12 @@
 // Endpoint for querying the fibonacci numbers
 
 import fibonacci from "./fib";
+import {Request, Response} from 'express';
 
-export default (req : { params: { num: string } }, res) => {
+export default (req : Request, res : Response) => {
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num)) as number;
+  const fibN = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
